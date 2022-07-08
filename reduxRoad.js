c366b436eed4e9b98f66c6ca851156f6d44527d8
Daniel Eldon <eldondaniel2@gmail.com>
2022-07-08 18:43:53 +0300

    case travel and payload

--- reduxRoad.js
+++ reduxRoad.js
@@ -5,6 +5,21 @@ const initialWagonState = {
   }
 const reducer = (state=initialWagonState, action) => {
     switch(action.type){
+        case 'gather': {
+            return {
+              ...state,
+              supplies: state.supplies + 15,
+              days: state.days + 1
+            }
+          }
+          case 'travel': {
+            return {
+              ...state,
+              supplies: state.supplies - 20,
+              distance: state.distance + 10,
+              days: state.distance + action.payload
+            }
+          }
         default: {
           return state;
         }
