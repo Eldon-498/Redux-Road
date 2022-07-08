67edb6f92abf5bc5cdbebc7141d3d9dc6f5b8c3a
Daniel Eldon <eldondaniel2@gmail.com>
2022-07-08 18:48:27 +0300

    tippedWagon case

--- reduxRoad.js
+++ reduxRoad.js
@@ -15,9 +15,16 @@ const reducer = (state=initialWagonState, action) => {
           case 'travel': {
             return {
               ...state,
-              supplies: state.supplies - 20,
+              supplies: state.supplies - (20*action.payload),
               distance: state.distance + 10,
               days: state.distance + action.payload
+            }
+          }
+          case 'tippedWagon': {
+            return {
+              ...state,
+              supplies: state.supplies - 30,
+              days: state.days + 1
             }
           }
         default: {
